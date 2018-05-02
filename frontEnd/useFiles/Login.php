718c4db67bd7d6cec9bb37eeea217c5e72db0e5c
bendecre <bendecre@comcast.net>
2018-05-02 19:58:03 -0400

    Login now validates against the DB correctly

--- frontEnd/useFiles/Login.php
+++ frontEnd/useFiles/Login.php
@@ -1,63 +1,69 @@
 <!DOCTYPE html>
 <?php
-// server should keep session data for AT LEAST 1 hour
-ini_set('session.gc_maxlifetime', 3600);
+	// server should keep session data for AT LEAST 1 hour
+	ini_set('session.gc_maxlifetime', 3600);
 
-// each client should remember their session id for EXACTLY 1 hour
-session_set_cookie_params(3600);
-session_start();
-
-// Connect to the SQLite database
-
-$db = new PDO("sqlite:../db/447db.sqlite") or die("Unable to open the database.");
-
-/* This will display the accounts table.
-$query = "SELECT * FROM accounts WHERE 1";
-foreach ($db->query($query) as $row){
-	echo $row[0];
-}
-
-$db = null;
-*/
-
-// Checks to see if the user is logged in, if so it redirects them to homepage
-if (isset($_SESSION["HAS_LOGGED_IN"])) {
-    if ($_SESSION["HAS_LOGGED_IN"]) {
-        header('Location: homepage.php');
-    }
-}
-
-if ($_POST) {
-    $username = strtolower($_POST["username"]);
-	$password = md5($_POST["password"]); //replace md5 with name of server
-
-    // Search if user email exists in DB
-    $search_account = "SELECT * FROM accounts WHERE userName = '$username' AND password = '$password'";
-
-	$row = $db->query($search_account);
+	// each client should remember their session id for EXACTLY 1 hour
+	session_set_cookie_params(3600);
+	session_start();
 	
-    // Check whether or not there has been a successful user creation
-    if ($row) {
-        session_start();
+	// Connect to the SQLite database
+	$db = new PDO("sqlite:../db/447db.sqlite") or die("Unable to open the database.");
+
+	/* This will display the accounts table.
+	$query = "SELECT * FROM accounts WHERE 1";
+	foreach ($db->query($query) as $row){
+		echo $row[0];
+	}
+
+	$db = null;
+	*/
+
+	// Checks to see if the user is logged in, if so it redirects them to homepage
+
+	/*
+	if (isset($_SESSION["HAS_LOGGED_IN"])) {
+		if ($_SESSION["HAS_LOGGED_IN"]) {
+			header('Location:../index.html');
+		}
+	}
+	*/
+
+	if ($_POST) {
+		$username = ($_POST["username"]);
+		$password = ($_POST["password"]); //replace md5 with name of server
+		//$password = md5($_POST["password"]); //replace md5 with name of server
+		// Search if user exists in DB
+		$search_account = "SELECT * FROM accounts WHERE userName = '$username' AND password = '$password';";
 		
-        // Translate the SQL Query into a dictionary
-        $advisorDict = mysqli_fetch_assoc($queryOfSearchAdvisor);
+		$counter = 0;
+		
+		foreach ($db->query($search_account) as $row){
+			$counter = $counter + 1;
+		}	
+		
+		// Check whether or not there has been a successful user creation
+		if ($counter == 1) {
+			/*
+			// Translate the SQL Query into a dictionary
+			$advisorDict = mysqli_fetch_assoc($queryOfSearchAdvisor);
 
-        // Assigning to session values based on what data is found
-        $_SESSION["HAS_LOGGED_IN"] = true;
-		//$_SESSION["SCHEDULE_VIEW"] = false;
-        $_SESSION["ADVISOR_EMAIL"] = $advisorDict["email"];
-        $_SESSION["ADVISOR_ID"] = $advisorDict["advisorID"];
-        $_SESSION["ADVISOR_FNAME"] = $advisorDict["firstName"];
-        $_SESSION["ADVISOR_LNAME"] = $advisorDict["lastName"];
+			// Assigning to session values based on what data is found
+			$_SESSION["HAS_LOGGED_IN"] = true;
+			//$_SESSION["SCHEDULE_VIEW"] = false;
+			$_SESSION["ADVISOR_EMAIL"] = $advisorDict["email"];
+			$_SESSION["ADVISOR_ID"] = $advisorDict["advisorID"];
+			$_SESSION["ADVISOR_FNAME"] = $advisorDict["firstName"];
+			$_SESSION["ADVISOR_LNAME"] = $advisorDict["lastName"];
+			*/
+			// Redirecting to index.html
+			//header('Location:../index.html');
+			echo "Login Successful";
+	} else {
+			echo "Login FAILED";
+	}
 
-        // Redirecting to index.html
-        header('Location:../index.html');
-    } else {
-        echo "Login FAILED";
-    }
-
-    $open_connection->close();
+   $db = null;
 }
 ?>
 <html>
